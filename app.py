9b4e866ad9ba767764a01c8c49e2b414af75b918
oruskade <orus-kade@yandex.ru>
2020-06-13 18:31:03 +0300

    home page updated

--- app.py
+++ app.py
@@ -4,36 +4,43 @@ import forms
 from datetime import datetime
 import base64
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///face.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
 class Embeddings(db.Model):
     __tablename__ = 'embeddings'
-    __table_args__ = { 'extend_existing': True }
+    __table_args__ = {'extend_existing': True}
     # LOC_CODE = db.Column(db.Integer, primary_key=True)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(100), nullable=False)
     second_name = db.Column(db.String(100), nullable=False)
+
     # person_embedding = db.Column(db.Text, nullable=False)
     # data_add = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Embeddings %r>' % self.id
 
+
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['POST', 'GET'])
 def index():
     return render_template("index.html")
 
 
 @app.route('/about')
 def about():
+    if request.method == "POST":
+        b64_string = request.form['cam_photo']
+        image_data = bytes(b64_string, encoding="ascii")
+        # TODO добавить идентификатор для файла
+        with open("temp_photos/imageToSave.jpg", "wb") as fh:
+            fh.write(base64.decodebytes(image_data))
     return render_template("about.html")
+
 
 @app.route('/person/add', methods=['POST', 'GET'])
 def add_person():
@@ -61,6 +68,7 @@ def saved_persons():
     persons = Embeddings.query.order_by(Embeddings.second_name).all()
     return render_template("saved_persons.html", persons=persons, deleted=deleted)
 
+
 @app.route('/person/delete/<int:id>')
 def delete_person(id):
     person = Embeddings.query.get_or_404(id)
@@ -70,6 +78,7 @@ def delete_person(id):
         return redirect("/person/saved?deleted=True")
     except:
         return "При удалении произошла ошибка!"
+
 
 @app.route('/person/edit/<int:id>', methods=['POST', 'GET'])
 def edit_person(id):
@@ -85,6 +94,7 @@ def edit_person(id):
     else:
         return render_template("edit_person.html", person=person, edited=False)
 
+# Можно убрать, было для тестов
 @app.route('/cam', methods=['POST', 'GET'])
 def cam_func():
     if request.method == "POST":
@@ -96,6 +106,7 @@ def cam_func():
         with open("temp_photos/imageToSave.jpg", "wb") as fh:
             fh.write(base64.decodebytes(image_data))
     return render_template("webcam_exp.html")
+
 
 # @app.route('/user/<string:name>/<int:id>')
 # def user(name, id):
